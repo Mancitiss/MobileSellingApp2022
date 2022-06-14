4afb2fba78be0152a9550d1f9bbe558170cdf375
Lspau <71479445+Lspau@users.noreply.github.com>
2022-06-14 23:45:10 +0700

    update

--- TestApplication/app/src/main/java/org/duckdns/mancitiss/testapplication/Account2.java
+++ TestApplication/app/src/main/java/org/duckdns/mancitiss/testapplication/Account2.java
@@ -58,32 +58,33 @@ public class Account2 extends AppCompatActivity {
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*
-                Intent intent2 = new Intent(Account2.this, MainActivity.class);
-                startActivity(intent2);
-                Toast.makeText(Account2.this, "you clicked OK button", Toast.LENGTH_SHORT).show();
-                */
-                String opw = ((EditText)findViewById(R.id.etx_pass)).getText().toString();
-                String npw = ((EditText)findViewById(R.id.etx_repass)).getText().toString();
-                String rnpw = ((EditText)findViewById(R.id.etx_confirm)).getText().toString();
-                if (!opw.isEmpty() && !npw.isEmpty() && !rnpw.isEmpty() && npw.equals(rnpw) && Connection.Companion.ChangePassword(Account2.this, Account2.this, opw, npw)){
-                    Toast.makeText(Account2.this, "Mật khẩu thay đổi thành công", Toast.LENGTH_SHORT).show();
-                    Intent intent2 = new Intent(Account2.this, Account.class);
-                    intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(intent2);
-                }
-                else {
-                    if (opw.isEmpty())
-                        Toast.makeText(Account2.this, "Vui lòng nhập mật khẩu hiện tại", Toast.LENGTH_SHORT).show();
-                    else if (npw.isEmpty())
-                        Toast.makeText(Account2.this, "Vui lòng nhập mật khẩu mới", Toast.LENGTH_SHORT).show();
-                    else if (rnpw.isEmpty())
-                        Toast.makeText(Account2.this, "Vui lòng xác nhận mật khẩu mới", Toast.LENGTH_SHORT).show();
-                    else if (!npw.equals(rnpw))
-                        Toast.makeText(Account2.this, "Mật khẩu xác nhận không khớp với mật khẩu mới", Toast.LENGTH_SHORT).show();
-                    else
-                        Toast.makeText(Account2.this, "Mật khẩu hiện tại không đúng", Toast.LENGTH_SHORT).show();
-                }
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String opw = ((EditText)findViewById(R.id.etx_pass)).getText().toString();
+                        String npw = ((EditText)findViewById(R.id.etx_repass)).getText().toString();
+                        String rnpw = ((EditText)findViewById(R.id.etx_confirm)).getText().toString();
+                        if (!opw.isEmpty() && !npw.isEmpty() && !rnpw.isEmpty() && npw.equals(rnpw) && Connection.Companion.ChangePassword(Account2.this, Account2.this, opw, npw)){
+                            Toast.makeText(Account2.this, "Mật khẩu thay đổi thành công", Toast.LENGTH_SHORT).show();
+                            Intent intent2 = new Intent(Account2.this, Account.class);
+                            intent2.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent2);
+                        }
+                        else {
+                            if (opw.isEmpty())
+                                Toast.makeText(Account2.this, "Vui lòng nhập mật khẩu hiện tại", Toast.LENGTH_SHORT).show();
+                            else if (npw.isEmpty())
+                                Toast.makeText(Account2.this, "Vui lòng nhập mật khẩu mới", Toast.LENGTH_SHORT).show();
+                            else if (rnpw.isEmpty())
+                                Toast.makeText(Account2.this, "Vui lòng xác nhận mật khẩu mới", Toast.LENGTH_SHORT).show();
+                            else if (!npw.equals(rnpw))
+                                Toast.makeText(Account2.this, "Mật khẩu xác nhận không khớp với mật khẩu mới", Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(Account2.this, "Mật khẩu cũ không đúng", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                thread.start();
             }
         });
 
